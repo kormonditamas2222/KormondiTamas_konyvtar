14fee14a37440ba55b7e69f30dc155bc63ca8858
kormonditamas2222 <t.kormond2006@gmail.com>
2025-03-12 19:05:06 +0100

    kész

--- konyvtar/Program.cs
+++ konyvtar/Program.cs
@@ -11,20 +11,36 @@
             books.Add(book2);
             Book book3 = new("A szürke ötven árnyalata", "nem tudom", 200, 2003);
             books.Add(book3);
-            string dolog = book1.ToString();
-            Console.WriteLine();
+            Console.WriteLine(book1.GetBookByTitle(books, "Egri csillagok"));
+            Console.WriteLine(book1.GetBookByTitle(books, "Gulliver utazásai"));
             foreach (Book book in books) { Console.WriteLine(book); }
             InputStuff(books);
+            foreach (Book book in books) { Console.WriteLine(book); }
         }
         static void InputStuff(List<Book> books)
         {
             int whichBook = -1;
+            string choice = string.Empty;
             while (!(whichBook >= 0 && whichBook < books.Count))
             {
                 Console.Write($"Melyik könyvet szeretnéd módosítani: (0-{books.Count - 1}) ");
                 whichBook = Convert.ToInt32(Console.ReadLine());
             }
-
+            while (!(choice == "author" || choice == "title" || choice == "pagecount" || choice == "releaseyear"))
+            {
+                Console.Write($"Melyik paraméterét szeretnéd megváltoztatni {books[whichBook].Title} könyvnek: (author, title, pagecount, releaseyear) ");
+                choice = Console.ReadLine().ToLower();
+            }
+            if (choice == "author" || choice == "title")
+            {
+                Console.Write("Add meg az új értéket");
+            }
+            else if (choice == "pagecount" || choice == "releaseyear")
+            {
+                Console.Write("Add meg az új értéket (kérlek számban :>)");
+            }
+            string input = Console.ReadLine();
+            books[0].EditBookData(books, whichBook, choice, input);
         }
     }
 }
