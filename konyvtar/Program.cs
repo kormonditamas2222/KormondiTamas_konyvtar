ec62fd6960fc9c36d41297ef489f3ee55bd12f80
kormonditamas2222 <t.kormond2006@gmail.com>
2025-03-12 18:24:49 +0100

    példányosítás

--- konyvtar/Program.cs
+++ konyvtar/Program.cs
@@ -2,9 +2,15 @@
 {
     internal class Program
     {
+        static List<Book> books = new();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Book book1 = new("Egri csillagok", "Gárdonyi Géza", 400, 1964);
+            books.Add(book1);
+            Book book2 = new("Harry Potter a Petrikben", "J. K. Rowling", 1000, 2025);
+            books.Add(book2);
+            Book book3 = new("A szürke ötven árnyalata", "nem tudom", 200, 2003);
+            books.Add(book3);
         }
     }
 }
